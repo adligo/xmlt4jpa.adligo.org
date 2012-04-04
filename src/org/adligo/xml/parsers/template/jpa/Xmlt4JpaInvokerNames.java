a492fdb9bb3bee1bf23defe505de12500399f034
scott <scott@adligo.com>
2012-04-04 02:24:27 +0000

    added transaction only wrapper

--- src/org/adligo/xml/parsers/template/jpa/Xmlt4JpaInvokerNames.java
+++ src/org/adligo/xml/parsers/template/jpa/Xmlt4JpaInvokerNames.java
@@ -2,12 +2,16 @@ package org.adligo.xml.parsers.template.jpa;
 
 
 public class Xmlt4JpaInvokerNames {
+	private static final String PACKAGE = "org.adligo.xml.parsers.template.jpa.";
 	/**
 	 * a I_GCheckedInvoker<ModifyEntityJpaRequest, Integer>
 	 * used to update (insert, update or delete statements) 
 	 * a database
 	 */
-	public static final String MODIFIER = "modifier";
-	
+	public static final String MODIFIER = PACKAGE + "modifier";
+	/**
+	 * same as MODIFIER in a Transaction wrapper
+	 */
+	public static final String MODIFIER_IN_TRANSACTION = PACKAGE + "modifier_in_transaction";
 	private Xmlt4JpaInvokerNames() {}
 }
--- src/org/adligo/xml/parsers/template/jpa/Xmlt4JpaRegistry.java
+++ src/org/adligo/xml/parsers/template/jpa/Xmlt4JpaRegistry.java
@@ -1,14 +1,18 @@
 package org.adligo.xml.parsers.template.jpa;
 
 import org.adligo.i.adig.client.GRegistry;
+import org.adligo.i.storage.StorageWrappers;
 
 public class Xmlt4JpaRegistry {
 	private static boolean isSetupUp = false;
 	
+	@SuppressWarnings("unchecked")
 	public static synchronized void setup() {
 		if (!isSetupUp) {
 			isSetupUp = true;
 			GRegistry.addCheckedInvoker(Xmlt4JpaInvokerNames.MODIFIER, new ModifyInvoker());
+			GRegistry.addCheckedInvoker(Xmlt4JpaInvokerNames.MODIFIER_IN_TRANSACTION, 
+					StorageWrappers.createTransactionWrapper(new ModifyInvoker()));
 		}
 	}
 }
