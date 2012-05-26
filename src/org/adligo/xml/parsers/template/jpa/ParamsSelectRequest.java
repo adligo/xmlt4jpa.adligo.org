194faee829eaa4a13dd657fc91ef913cc97d16b0
scott <scott@adligo.com>
2012-05-26 00:54:46 +0000

    working with limit offset

--- src/org/adligo/xml/parsers/template/jpa/ParamsSelectRequest.java
+++ src/org/adligo/xml/parsers/template/jpa/ParamsSelectRequest.java
@@ -5,13 +5,26 @@ import org.adligo.models.params.client.I_TemplateParams;
 
 public class ParamsSelectRequest extends SelectRequest {
 	private I_TemplateParams params;
-
+	/**
+	 * if true this should actually cause two queries 
+	 * one to count the rows the other to get the rows
+	 */
+	private boolean countRows = false;
+	
 	public I_TemplateParams getParams() {
 		return params;
 	}
 
 	public void setParams(I_TemplateParams params) {
 		this.params = params;
+	}
+
+	public boolean isCountRows() {
+		return countRows;
+	}
+
+	public void setCountRows(boolean countRows) {
+		this.countRows = countRows;
 	}
 	
 }
